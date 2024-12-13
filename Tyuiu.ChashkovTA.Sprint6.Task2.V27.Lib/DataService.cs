239940b367782f2cc840b7e3523645abcabfb902
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 18:47:41 +0500

    Task2

--- Tyuiu.ChashkovTA.Sprint6.Task2.V27.Lib/DataService.cs
+++ Tyuiu.ChashkovTA.Sprint6.Task2.V27.Lib/DataService.cs
@@ -17,5 +17,26 @@ namespace Tyuiu.ChashkovTA.Sprint6.Task2.V27.Lib
 
             return functionValues;
         }
+        private double CalculateFunction(int x)
+        {
+            try
+            {
+                double numerator = 2 * x - 3;
+                double denominator = Math.Cos(x) - 2 * x;
+
+                if (denominator == 0)
+                {
+                    return 0; // Возвращаем 0 при делении на ноль
+                }
+
+                double result = (numerator / denominator) + 5 * x - Math.Sin(x);
+                return Math.Round(result, 2); // Округление до 2 знаков после запятой
+            }
+            catch (Exception)
+            {
+                return 0; // Обработка ошибок, если вдруг что-то пойдет не так
+            }
+        }
     }
 }
+
