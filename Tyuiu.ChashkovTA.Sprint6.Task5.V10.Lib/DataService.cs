f358bebf720c8aeb60d9352e05e00f7a63c52249
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 19:07:10 +0500

    Task5

---
+++ Tyuiu.ChashkovTA.Sprint6.Task5.V10.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.ChashkovTA.Sprint6.Task5.V10.Lib
+{
+    public class DataService : ISprint6Task5V10
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"Файл по пути {path} не найден.");
+            }
+
+            List<double> dataList = new List<double>();
+
+            foreach (var line in File.ReadLines(path))
+            {
+
+                string[] parts = line.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var part in parts)
+                {
+                    if (double.TryParse(part, out double value))
+                    {
+                        dataList.Add(Math.Round(value, 3));
+                    }
+                }
+            }
+
+            List<double> filteredData = new List<double>();
+            foreach (var data in dataList)
+            {
+                if (data != 0)
+                {
+                    filteredData.Add(data);
+                }
+            }
+
+            return filteredData.ToArray();
+        }
+    }
+}
---
+++ Tyuiu.ChashkovTA.Sprint6.Task5.V10.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.ChashkovTA.Sprint6.Task5.V10.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
