580e242474980521083069c62ee1b806b8adfebf
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 19:02:05 +0500

    Task4

--- Tyuiu.ChashkovTA.Sprint6.Task4.V17.Lib/DataService.cs
+++ Tyuiu.ChashkovTA.Sprint6.Task4.V17.Lib/DataService.cs
@@ -6,17 +6,16 @@ namespace Tyuiu.ChashkovTA.Sprint6.Task4.V17.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            int size = stopValue - startValue + 1;  // Размер массива
+            int size = stopValue - startValue + 1; 
             double[] result = new double[size];
 
             for (int i = 0; i < size; i++)
             {
                 int x = startValue + i;
 
-                // Проверка деления на ноль
                 if (x == 0)
                 {
-                    result[i] = 0;  // Если x = 0, то функция не определена, возвращаем 0
+                    result[i] = 0; 
                 }
                 else
                 {
@@ -24,7 +23,6 @@ namespace Tyuiu.ChashkovTA.Sprint6.Task4.V17.Lib
                     result[i] = (Math.Sin(x) / x) + 1.7 - (Math.Cos(x) * 4 * x) - 6;
                 }
 
-                // Округление до двух знаков после запятой
                 result[i] = Math.Round(result[i], 2);
             }
 
