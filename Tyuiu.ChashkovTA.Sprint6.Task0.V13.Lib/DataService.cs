68c7d801103e7b4aab86dda740b299037e915d2d
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 18:11:46 +0500

    Add project files.

---
+++ Tyuiu.ChashkovTA.Sprint6.Task0.V13.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.ChashkovTA.Sprint6.Task0.V13.Lib
+{
+    public class DataService : ISprint6Task0V13
+    {
+        public double Calculate(int x)
+        {
+            double numerator = Math.Pow(x, 3);
+            double denominator = Math.Pow(x, 2) - 1;
+
+            if (denominator == 0)
+            {
+                return 0;
+            }
+
+            double result = numerator / denominator;
+            return Math.Round(result, 3);
+        }
+    }
+}
