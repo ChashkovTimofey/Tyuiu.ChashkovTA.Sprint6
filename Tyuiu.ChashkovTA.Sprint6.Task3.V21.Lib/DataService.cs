43458092f4d8cd17c4b25c843c46785ae376b8b0
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 18:53:01 +0500

    Task3

--- Tyuiu.ChashkovTA.Sprint6.Task3.V21.Lib/DataService.cs
+++ Tyuiu.ChashkovTA.Sprint6.Task3.V21.Lib/DataService.cs
@@ -6,17 +6,15 @@ namespace Tyuiu.ChashkovTA.Sprint6.Task3.V21.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            int rows = matrix.GetLength(0);
-            int columns = matrix.GetLength(1);
+            int rows = matrix.GetLength(0); 
+            int columns = matrix.GetLength(1); 
 
-            // Сортировка строк массива по первому столбцу
             for (int i = 0; i < rows - 1; i++)
             {
                 for (int j = i + 1; j < rows; j++)
                 {
                     if (matrix[i, 0] > matrix[j, 0])
                     {
-                        // Меняем строки местами
                         for (int k = 0; k < columns; k++)
                         {
                             int temp = matrix[i, k];
